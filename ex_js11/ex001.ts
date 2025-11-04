60d321736f2c1ea72365ce3365435c98f716018d
MariPires96 <mariana.pires96@hotmail.com>
2025-11-04 20:35:15 -0300

    Conclusão do exercício 2 e alteração exercício 1

--- ex_js11/ex001.ts
+++ ex_js11/ex001.ts
@@ -6,7 +6,7 @@ const numeros: Array<number> = new Array<number>(2, 5, 1, 3, 4, 9, 7, 8, 10, 6);
 let numero: number;
 
 numero = readlineSync.questionInt('Digite um número inteiro: ',
-    {limitMessage: 'Entrada inválida. Digite um número inteiro.'});
+    {limitMessage: 'Entrada inválida. Digite um número inteiro'});
 
 const indiceEncontrado: number = numeros.indexOf(numero);
 
